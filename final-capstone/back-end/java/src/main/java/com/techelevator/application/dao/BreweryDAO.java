383faf050fff72904f413e38387b61992b95912e
Samantha Lemke <Sjlemke22@gmail.com>
2021-04-05 18:23:37 -0400

    cap

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/BreweryDAO.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/BreweryDAO.java
@@ -1,9 +1,17 @@
 package com.techelevator.application.dao;
 
+import com.techelevator.application.model.Brewery;
+
 public interface BreweryDAO {
 	Brewery getBreweryByZipCode(String zipCode);
 	
+	Brewery getBreweryByName(String name);
 	
+	Brewery getBreweryByCityState(String city, String state);
+	
+	Brewery getBreweryByCity(String city);
+	
+	Brewery getBreweryByState(String state);
 
 		
 }
--- final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/BrewerySQLDAO.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/dao/BrewerySQLDAO.java
@@ -1,5 +1,46 @@
 package com.techelevator.application.dao;
 
-public class BrewerySQLDAO {
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.techelevator.application.model.Brewery;
+
+public class BrewerySQLDAO implements BreweryDAO {
+	private JdbcTemplate jdbcTemplate;
+
+    public BrewerySQLDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+	@Override
+	public Brewery getBreweryByZipCode(String zipCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Brewery getBreweryByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Brewery getBreweryByCityState(String city, String state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Brewery getBreweryByCity(String city) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Brewery getBreweryByState(String state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
+	
 
 }
