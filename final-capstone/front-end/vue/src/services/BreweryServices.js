7ac7f88dcb8c5399d450884c4867dca03234bc2b
Eric Dahl <dahlor@gmail.com>
2021-04-05 22:40:45 -0400

    BreweryServices added.

---
+++ final-capstone/front-end/vue/src/services/BreweryServices.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: "http://localhost:8080"
+});
+
+export default {
+
+      getBreweriesByName(name) {
+        return http.get(`/breweries/name/${name}`);
+      },
+
+      getBreweriesByCity(city) {
+        return http.get(`/breweries/name/${city}`);
+      },
+
+      getBreweriesByState(state) {
+        return http.get(`/breweries/name/${state}`);
+      },
+
+      getBreweriesByZipCode(zipCode) {
+        return http.get(`/breweries/name/${zipCode}`);
+      },
+
+      getBreweriesByName() {
+        return http.get('/topics');
+      }
+  }
