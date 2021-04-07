928189602895e49198c46d1f0c8485579751f9f4
Patrick Cooper <pcooper4117@gmail.com>
2021-04-07 10:47:34 -0400

    update

---
+++ final-capstone/front-end/vue/src/components/NewBrewrery.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<h1> Placeholder Text</h1>>
+<div id="NewBreweryForm">
+
+<form> 
+    <input type="text" v-model="inputName">
+    <input type="text" v-model="inputStreet">
+    <input type="text" v-model="inputCity">
+    <input type="text" v-model="inputState">
+    <input type="text" v-model="inputZip">
+    <input type="text" v-model="inputURL">
+    <input type="text" v-model="inputPhoneNumber">
+    <input type="text" v-model="inputBreweryUserName">
+ </form>
+
+
+
+</div>
+</template>
