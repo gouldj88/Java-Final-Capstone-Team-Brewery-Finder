23b3050045edfe153d92da02ce5c72e8230bf56f
Samantha Wochna <samanthawochna@gmail.com>
2021-04-08 15:12:14 -0400

    making it pretty

--- final-capstone/front-end/vue/src/App.vue
+++ final-capstone/front-end/vue/src/App.vue
@@ -1,21 +1,20 @@
 <template>
-<body>
   <div id="app">
+    <body>
     <header>
-        <div id="nav">
-          <!--<router-link v-bind:to="{ name: 'home' }">Back To Search</router-link>&nbsp;|&nbsp;-->
+        <div id="nav">         
+          <img src="./assets/hops.png" style="width: 30px">
+          <span> Brewery Finder </span>
+          <router-link v-bind:to="{ name: 'home' }">Find A Brewery</router-link>&nbsp;|&nbsp;
           <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;|&nbsp;
           <router-link v-bind:to="{ name: 'addBrewery' }">Add New Brewery</router-link>
-        
+          
+ 
         </div>
-        </header>
-      
-
-    
+    </header>
 
     <article class="container">
-       <router-view />
-       <h1>HI</h1>
+      <router-view />
     </article>
 
     <aside class="sidebar-left">
@@ -29,56 +28,81 @@
 
     <footer>
     </footer>
-  </div>
+
 </body>
+</div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Chango&display=swap');
+
 body{
-  display: grid;
-    grid-template-columns: 100% 100% 100% ;
+  display: grid;  
+  grid-gap: 20px;
+  grid-template-columns: 15% 67.5% 15% ;
+  grid-template-rows: 10% 85% 10%;
+  height: 100vh;
 
  grid-template-areas:
- "header header header"
- "sidebar-left  container sidebar-right"
- "footer footer footer"
+    "header         header        header"
+    "sidebar-left   container     sidebar-right"
+    "footer         footer        footer";
 }
 header {
-  grid-area: header;
-    background-color: rgb(138, 106, 0);
-    width: 100%;
-}
+    grid-area: header;
+    background-color: rgb(46, 81, 155);   
+    padding: 10px;
+    color: white;
+    display: flex;
+    justify-content: left;
+    align-items: center; 
 
+}
 .container {
   grid-area: container;
-  background-color: rgb(175, 44, 34);
+  background-color: rgb(255, 255, 255);
   justify-content: center;
-  width: 66%;
-  
+  align-items: center;
+  text-align: center;
+
 }
 
 .sidebar-left {
   grid-area: sidebar-left;
-  background-color: rgb(39, 138, 0);
-  width: 10%;
-  float: left;
-  height: 236%;
-  
-  
+  background-color: white;
+  align-items: center;
+  text-align: center;
 }
 
 .sidebar-right {
   grid-area: sidebar-right;
-  background-color: rgb(138, 106, 0);
-  width: 10%;
-  float: right;
-  height: 236%;
+  background-color: white;
+  align-items: center;
+  text-align: center;
 }
 
 footer {
-  background-color: rgb(138, 106, 0);
-  height: 50px;
+  grid-area: footer;
+  background-color: rgb(0, 83, 138);
+  padding: 10px;
+  margin-bottom: 0px;
+}
+
+span {
+  font-family: chango;
+  font-size: 40px ;
+}
+
+img {
+  align-items: center;
+}
+
+a:active, a:visited {
+  color: white;
+  text-decoration: none;
 }
 
 
+
 </style>
+
--- final-capstone/front-end/vue/src/views/Home.vue
+++ final-capstone/front-end/vue/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <h1>Brewery Finder</h1>
+    <h1>Search For a Brewery</h1>
     <p>Find breweries based on your search type</p>
     <brewery-list />
   </div>
