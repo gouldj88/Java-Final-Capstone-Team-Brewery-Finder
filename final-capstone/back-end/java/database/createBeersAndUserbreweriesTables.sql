f9e3d0fd16d4dab94bdb2c9e554bb8d692264808
Jeff Gould <gould.j88@gmail.com>
2021-04-13 11:53:54 -0400

    reviews table

--- final-capstone/back-end/java/database/createBeersAndUserbreweriesTables.sql
+++ final-capstone/back-end/java/database/createBeersAndUserbreweriesTables.sql
@@ -69,3 +69,15 @@ insert into beers (obdb_id, beer_name, brewery, description, image, abv, beer_ty
 
 
 insert into userbrewerydetails (obdb_id, history, image_url, hour_open, hour_closed, open_sun, open_mon, open_tue, open_wed, open_thu, open_fri, open_sat) values ('jeffs-tavern-and-brewhouse', 'Jeff''s Tavern & Brewhouse has been a Sandusky staple for over a century. Come try the tasty Woodstock Express cocktail. Or the Corkscrew, if you''re into pain.', 'https://meltbarandgrilled.com/wp-content/gallery/gallery-location-melt-cedar-point/MELT_LOCATION_CEDAR_POINT_004.jpg', '11:00 AM', '11:00 PM', true, false, false, true, true, true, true);
+
+drop table if exists reviews cascade;
+
+CREATE TABLE reviews
+(
+review_id serial primary key unique,
+beer_id int,
+review_text character varying (255),
+star_rating int,
+username character varying (255),
+constraint fk_beer_id foreign key (beer_id) references beers(beer_id)
+);
